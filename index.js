6feae466e3cf598e6f27f148513e758ef9195a80
Irina <kadushkina.exceedteam@yandex.ru>
2020-09-23 17:26:19 +0300

    mongoose

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
 const app = express();
 const jsonParser = express.json();
+const schema = require('./models/SchemaTodo')
+const bodyParser = require('body-parser')
 
-const url = 'mongodb://localhost:27017/';
 
-const mongo = new MongoClient(url, { useNewUrlParser: true });
+app.get("/tasks",jsonParser, schema.getTasks);
+app.post("/add",jsonParser, schema.addTask);
+app.post("/delete",jsonParser, schema.deleteTask);
+app.post("/check", jsonParser, schema.checkTask);
 
-mongo.connect((err, client) => {
-
-    app.locals.collection = client.db("Users_DB").collection("Task");
-    app.listen(8000, function () {
-        console.log("Сервер ожидает подключения...");
-    });
-
-})
-app.get("/api/users", function (req, res){
-    const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, tasks){
-        if(err) return console.log(err);
-        let list = tasks;
-        res.json({ list });
-    });
+app.listen(8000, function () {
+    console.log("Сервер ожидает подключения...")
 });
 
-app.post("/api/add",jsonParser, function (req, res) {
-    const {text} = req.body;
-    console.log('....', text);
-    if(!req.body) return res.sendStatus(400);
-    let task = { text, isCompleted: false }
-    const collection = req.app.locals.collection;
-    collection.insertOne(task);
-});
-
-app.post('/api/delete', function(req, res){
-    const {id} = req.body;
-    console.log('....', id);
-    const collection = req.app.locals.collection;
-    collection.deleteOne({ _id: id});
-});
-
+app.use(bodyParser.json())
